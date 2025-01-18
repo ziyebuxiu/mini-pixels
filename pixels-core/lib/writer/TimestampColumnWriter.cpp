24aafa541d976539c7bdc98673201e65591040b8
胡梦溪 <14808835+humengxi@user.noreply.gitee.com>
2025-01-18 15:36:58 +0800

    complete TimestampColumnWriter

--- pixels-core/lib/writer/TimestampColumnWriter.cpp
+++ pixels-core/lib/writer/TimestampColumnWriter.cpp
@@ -17,3 +17,124 @@
  * License along with Pixels.  If not, see
  * <https://www.gnu.org/licenses/>.
  */
+
+#include "writer/TimestampColumnWriter.h"
+#include "utils/BitUtils.h"
+
+TimestampColumnWriter::TimestampColumnWriter(std::shared_ptr<TypeDescription> type, std::shared_ptr<PixelsWriterOption> writerOption)
+    : ColumnWriter(type, writerOption), curPixelVector(pixelStride)
+{
+    runlengthEncoding = encodingLevel.ge(EncodingLevel::Level::EL2);
+    if (runlengthEncoding)
+    {
+        encoder = std::make_unique<RunLenIntEncoder>(1, 1);
+    }
+}
+
+int TimestampColumnWriter::write(std::shared_ptr<ColumnVector> vector, int size)
+{
+    auto columnVector = std::static_pointer_cast<TimestampColumnVector>(vector);
+    if (!columnVector)
+    {
+        throw std::invalid_argument("Invalid vector type");
+    }
+    long *timestamps = columnVector->times;
+    int curPartLength;         // size of the partition which belongs to current pixel
+    int curPartOffset = 0;     // starting offset of the partition which belongs to current pixel
+    int nextPartLength = size; // size of the partition which belongs to next pixel
+    while ((curPixelIsNullIndex + nextPartLength) >= pixelStride)
+    {
+        curPartLength = pixelStride - curPixelIsNullIndex;
+        writeCurPartTimestamp(columnVector, timestamps, curPartLength, curPartOffset);
+        newPixel();
+        curPartOffset += curPartLength;
+        nextPartLength = size - curPartOffset;
+    }
+    return outputStream->getWritePos();
+}
+
+void TimestampColumnWriter::close()
+{
+    if (runlengthEncoding && encoder)
+    {
+        encoder->clear();
+    }
+    ColumnWriter::close();
+}
+
+void TimestampColumnWriter::writeCurPartTimestamp(std::shared_ptr<ColumnVector> columnVector, long *values, int curPartLength, int curPartOffset)
+{
+    for (int i = 0; i < curPartLength; i++)
+    {
+        if (columnVector->isNull[i + curPartOffset])
+        {
+            curPixelVector[curPixelVectorIndex++] = 0;
+        }
+        else
+        {
+            curPixelVector[curPixelVectorIndex++] = values[i + curPartOffset];
+        }
+    }
+    std::copy(columnVector->isNull + curPartOffset, columnVector->isNull + curPartOffset + curPartLength, isNull.begin() + curPixelIsNullIndex);
+    curPixelIsNullIndex += curPartLength;
+}
+
+
+void TimestampColumnWriter::newPixel()
+{
+    if (runlengthEncoding)
+    {
+        std::vector<byte> buffer(curPixelVectorIndex * sizeof(int));
+        int resLen;
+        encoder->encode(curPixelVector.data(), buffer.data(), curPixelVectorIndex, resLen);
+        outputStream->putBytes(buffer.data(), resLen);
+    }
+    else
+    {
+        std::cout << "no runlength encoding" << std::endl;
+        std::shared_ptr<ByteBuffer> curVecPartitionBuffer;
+        EncodingUtils encodingUtils;
+        curVecPartitionBuffer = std::make_shared<ByteBuffer>(curPixelVectorIndex * sizeof(int));
+        if (byteOrder == ByteOrder::PIXELS_LITTLE_ENDIAN)
+        {
+            for (int i = 0; i < curPixelVectorIndex; i++)
+            {
+                encodingUtils.writeIntLE(curVecPartitionBuffer, curPixelVector[i]);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < curPixelVectorIndex; i++)
+            {
+                encodingUtils.writeIntBE(curVecPartitionBuffer, curPixelVector[i]);
+            }
+        }
+        outputStream->putBytes(curVecPartitionBuffer->getBuffer(), curVecPartitionBuffer->size());
+    }
+
+    ColumnWriter::newPixel();
+}
+
+pixels::proto::ColumnEncoding TimestampColumnWriter::getColumnChunkEncoding()
+{
+    pixels::proto::ColumnEncoding columnEncoding;
+    if (runlengthEncoding)
+    {
+        columnEncoding.set_kind(pixels::proto::ColumnEncoding::Kind::ColumnEncoding_Kind_RUNLENGTH);
+    }
+    else
+    {
+        columnEncoding.set_kind(pixels::proto::ColumnEncoding::Kind::ColumnEncoding_Kind_NONE);
+    }
+    return columnEncoding;
+}
+
+
+bool TimestampColumnWriter::decideNullsPadding(std::shared_ptr<PixelsWriterOption> writerOption)
+{
+    if (writerOption->getEncodingLevel().ge(EncodingLevel::Level::EL2))
+    {
+        return false;
+    }
+    return writerOption->isNullsPadding();
+}
